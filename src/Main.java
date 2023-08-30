bba2545ae1ad1bc035d7f6f0384b9bcb4321c8a4
Doğa Güneş <dogagunes0702@gmail.com>
2023-08-30 18:43:41 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,11 @@ public class Main {
 
         System.out.println(mis.isCompleted);
         cha.addZombieToPlace(1);
-        //cha.addCharactersToPlaces();
-        //cha.addCharactersToPlaces();
+       // cha.addCharactersToPlaces();
         cha.showSelections(1);
         System.out.println(mis.isCompleted);
         cha2.addZombieToPlace(2);
-        //cha.addCharactersToPlaces();
+       // cha.addCharactersToPlaces();
         cha2.showSelections(2);
 
         /*
