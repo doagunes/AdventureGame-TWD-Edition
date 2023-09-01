a5097538c18303798943c5fd3bac49e0fab7534e
benhur-okur <117393881+benhur-okur@users.noreply.github.com>
2023-09-01 22:14:36 +0300

    Inittial main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,5 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("benhur <3 dogi");
-        System.out.println("çakılişkooooo <3");
-        System.out.println("smkldnskdjfn");
-        
     }
 }
