3e145590ee5f76181051419230b76e94d7194daf
benhur-okur <117393881+benhur-okur@users.noreply.github.com>
2023-09-01 19:06:51 +0300

    character selection finished

--- src/MainCharacter.java
+++ src/MainCharacter.java
@@ -1,9 +1,18 @@
 public class MainCharacter extends Character{
-    Character selectCharacter;
+    private Character selectCharacter;
     public MainCharacter(String name, Inventory inventory, double power,
                          double criticalRate, double defenseRate, String goodQuote, String badQuote) {
         super(name, inventory, power, criticalRate, defenseRate, goodQuote, badQuote);
+        selectCharacter = null;
         health = 300;
+    }
+
+    public Character getSelectCharacter() {
+        return selectCharacter;
+    }
+
+    public void setSelectCharacter(Character selectCharacter) {
+        this.selectCharacter = selectCharacter;
     }
 
     @Override
